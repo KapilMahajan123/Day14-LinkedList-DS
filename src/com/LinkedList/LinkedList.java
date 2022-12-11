0b18384b00dba7f3dc3fa2999a0269d1e7249a80
kapilmahajan123 <montyempire123gmail.com>
2022-12-11 13:21:51 +0530

    Added: Ability to sequence of last

--- src/com/LinkedList/LinkedList.java
+++ src/com/LinkedList/LinkedList.java
@@ -18,7 +18,7 @@ public class LinkedList {
 		}
 	}
 	/*
-	 * Create list method
+	 * ADD - FIRST POSITION
 	 * create new node to be added
 	 */
 	public void addFirst(Object data) 
@@ -26,6 +26,29 @@ public class LinkedList {
 			Node newNode = new Node(data);    //new node with given data 
 			newNode.next = head;
 			head = newNode;
+		}
+	
+	 /*
+	  * ADD - LAST POSITION
+	  * if the linked list empty.
+	  * then make the new node as head
+	  */
+	public void addLast(Object data)
+		{
+			Node newNode = new Node(data);
+			Node temp = head;
+			if(head == null) {
+			head = newNode;
+		}
+			
+		else
+			{
+				while(temp.next != null)
+				{
+					temp = temp.next;
+				}
+				temp.next = newNode;
+			}
 		}
 	/*
 	 * Display position
@@ -48,11 +71,20 @@ public class LinkedList {
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 		System.out.println("Welcome to Linked List Problem");
+		System.out.println("Adding Nodes At First");
 		list.addFirst(70);
 		list.display();
 		list.addFirst(30);
 		list.display();
 		list.addFirst(56);
+		list.display();
+		System.out.println();
+		System.out.println("Adding Nodes At last");
+		list.addLast(56);
+		list.display();
+		list.addLast(30);
+		list.display();
+		list.addLast(70);
 		list.display();
 		
 	}
